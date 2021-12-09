d33ea2a069d65938be91ad6cfdc5fb215ac7f301
KennethT404 <tankenneth98@gmail.com>
2021-12-08 20:52:10 -0500

    change to maintf teraform block

--- main.tf
+++ main.tf
@@ -6,7 +6,17 @@ terraform {
     }
   }
   required_version = "> 0.14"
+
+  backend "s3" {
+      bucket = "circle-ci-backend-20211209014733840800000001"
+      key    = "terraform/webapp/terraform.tfstate"    
+      region = "us-east-1" 
+      }
+
 }
+
+
+
 
 provider "aws" {
   region = var.region
